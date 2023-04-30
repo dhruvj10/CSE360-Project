660ed33bd77d8856a1f7e7761978fe63c418ae76
BenjaminJones297 <92744018+BenjaminJones297@users.noreply.github.com>
2023-04-30 10:50:59 -0700

    Add files via upload

--- BackLogItem.java
+++ BackLogItem.java
@@ -1,26 +1,69 @@
-//package Prototype;
+package application;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class BackLogItem {
 	
+	/**********************************************************************************************************
+	 * int dif: This represents the estimated complexity on a scale of 1-10
+	 * int time: this represents the estimated time in hours to complete the backLogItem
+	 * String name: Is the name
+	 * ArrayList<Defect> defects: This represents all the defects that could be run into or have been run into
+	 * long timeElapsed: this represents the number of seconds this backLogItem has been worked on
+	 * String description: is the description
+	 **********************************************************************************************************/
+	
 	private int dif;
 	private int time;
 	private String name;
 	ArrayList<Defect> defects;
+	private long timeElapsed;
+	private String description;
 	
+	/******************************
+	 * Constructors, setters and getters
+	 ******************************/
 	
 	public BackLogItem() {
 		defects = new ArrayList<Defect>();
 		dif = 1;
 		time = 1;
 		name = "";
+		timeElapsed = 0;
+		description = "";
 	}
 	public BackLogItem(int d, int t, String n, ArrayList<Defect> dL) {
 		defects = new ArrayList<Defect>();
 		dif = d;
 		time = t;
 		name = n;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String d) {
+		description = d;
+	}
+	
+	public long getTimeE() {
+		return timeElapsed;
+	}
+	
+	/************************
+	 * Adds to timeElapsed
+	 * @param t
+	 */
+	public void addTime(long t) {
+		timeElapsed += t;
+	}
+	
+	/*
+	 * returns the estimated number of seconds remaining by subtracting time spent from estimated total time
+	 */
+	public long getTimeR() {
+		return (time * 60 * 60) - timeElapsed;
 	}
 	
 	public int getDif() {
@@ -47,28 +90,18 @@ public class BackLogItem {
 		dif = d;
 	}
 	
-	/*public void combineDefects(Defect d1, Defect d2, int dif) {
-		// simply combines 2 given defects
-		if (dif == 0) {
-			return;
-		}
-		Defect tempD = new Defect();
-		tempD.setName(d1.getName());
-		int avgD = (d1.getDif() + d2.getDif())/2;
-		tempD.setDif(avgD);
-		int avgT = (d1.getTime() + d2.getTime())/2;
-		tempD.setTime(avgT);
-		int avgR = (d1.getRec() + d2.getRec())/2;
-		tempD.setRec(avgR);
-		defects.remove(d1);
-		defects.remove(d2);
-		defects.add(tempD);
-	}*/
+	/*
+	 * Same as combineBackLogItems
+	 */
 	
 	public void combineDefects(ArrayList<Defect> l) {
 		// create new backlog item
 		Defect newDef = new Defect();
 		newDef.setName(l.get(0).getName());
+		newDef.setCause(l.get(0).getCause());
+		newDef.setSymptoms(l.get(0).getSymptoms());
+		newDef.setSymptoms(l.get(0).getSolution());
+		newDef.close();
 		int avgD = 0;
 		int avgT = 0;
 		int avgR = 0;
@@ -94,6 +127,7 @@ public class BackLogItem {
 		out += name;
 		out += "\n\tDifficulty: " + dif + "\\10";
 		out += "\n\tTime required: " + time + " hours.";
+		out += "Description: " + description;
 		out += "\n\tDefects:\n";
 		for (int i = 0; i < defects.size(); i ++) {
 			out += "\tCommon Issue #" + (i+1) + ": ";
